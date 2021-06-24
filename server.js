2801f2c14c0a88e8ed9d085f1a917d64b2520ff3
yumour <moh.abdessattar@gmail.com>
2021-06-24 23:26:51 +0100

    preparing for deployment
    removed header

--- server.js
+++ server.js
@@ -4,14 +4,7 @@ const path = require("path");
 const app = express();
 
 connectDb();
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, x-requested-with , Content-Type, Accept"
-  );
-  next();
-});
+
 app.use(express.json({ extended: true }));
 // Routeer
 app.use("/api/user", require("./routes/api/user"));
@@ -21,7 +14,7 @@ app.use("/api/profile", require("./routes/api/profile"));
 // server static
 if (process.env.NODE_ENV === "production") {
   // set
-  app.use(express.static("client/build"));
+  app.use(express.static("/client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
